f4af7365cb3c8b69a6dfa28e890b20321e8cf8e9
GyulaBujdoso <gbujdos1@gmail.com>
2023-01-05 19:33:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
+
+let arrow1 = document.querySelector("#arrow1");
+let arrow2 = document.querySelector("#arrow2");
+let arrow3 = document.querySelector("#arrow3");
+let arrow4 = document.querySelector("#arrow4");
+
+arrow1.addEventListener('click', () => {
+    btn1.click();
+});
+
+arrow2.addEventListener('click', () => {
+    btn2.click();
+});
+
+
+arrow3.addEventListener('click', () => {
+    btn3.click();
+});
+
+arrow4.addEventListener('click', () => {
+    btn4.click();
+});
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+}   ,800);
+
+});
